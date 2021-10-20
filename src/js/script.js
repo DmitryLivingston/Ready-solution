18305ad98c6e22423896633f849375d8ea9385f3
DmitryLivingston <Zaycheg19@mail.ru>
2021-10-20 11:45:08 +0900

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider').slick();
+    $('.partners_slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });
+});
