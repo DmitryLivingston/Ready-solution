690800151b0129c08ed318fefcc012d03d25a889
Dmitry Samsonov <63533381+DmitryLivingston@users.noreply.github.com>
2021-10-20 23:58:31 +0900

    Адаптив слайдера партнеры

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,31 @@ $(document).ready(function(){
     $('.partners_slider').slick({
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
 });
